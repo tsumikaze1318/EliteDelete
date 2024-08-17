360cf1c88e7d01fcbe5c9ecdfa581c2aab5d0e78
青木悠人 <TTC22C1020001@edu.tech.ac.jp>
2024-08-17 19:04:30 +0900

    強制スクロールできたよ

--- Assets/Member/Aoki/Scripts/BackScroll.cs
+++ Assets/Member/Aoki/Scripts/BackScroll.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 public class BackScroll : MonoBehaviour
 {
-    private float length, startpos;
-    [SerializeField] private GameObject cam;
-    [SerializeField] private float parallaxEffect;
+    [SerializeField] private float scrollSpeed = 2.0f;  // スクロールの速度
+    private bool isScrolling = true;
 
-    void Start()
+    void Update()
     {
-        startpos = transform.position.x;
-        //length = GetComponent<SpriteRenderer>().bounds.size.x;
-    }
-    private void FixedUpdate()
-    {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
-        transform.position = new Vector3(startpos + dist,transform.position.y,transform.position.z);
-        if (temp > startpos + length) startpos += length;
-        else if (temp < startpos - length) startpos -= length;
+        if(isScrolling)
+        {
+            transform.position += new Vector3(scrollSpeed * Time.deltaTime, 0, 0);
+            if(transform.position.x >= 100)
+            {
+                isScrolling = false;
+            }
+        }
     }
 }
+
+
+    //private float length, startpos;
+    //[SerializeField] private GameObject cam;
+    //[SerializeField] private float parallaxEffect;
+
+    //void Start()
+    //{
+    //    startpos = transform.position.x;
+    //    //length = GetComponent<SpriteRenderer>().bounds.size.x;
+    //}
+    //private void FixedUpdate()
+    //{
+    //    float temp = (cam.transform.position.x * (1 - parallaxEffect));
+    //    float dist = (cam.transform.position.x * parallaxEffect);
+    //    transform.position = new Vector3(startpos + dist,transform.position.y,transform.position.z);
+    //    if (temp > startpos + length) startpos += length;
+    //    else if (temp < startpos - length) startpos -= length;
+    //}
