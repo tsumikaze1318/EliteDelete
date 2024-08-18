1f1d86720f5903177371b2d78e8580bd8ee6b571
Shota Nakahari <fire.stnr@icloud.com>
2024-08-18 09:43:23 +0900

    その他もろもろ微調整

--- Assets/Member/Aoki/Scripts/GameScore.cs
+++ Assets/Member/Aoki/Scripts/GameScore.cs
@@ -9,6 +9,6 @@ public class GameScoreDisplay : MonoBehaviour
     {
         // 現在のスコアを取得して表示
         int currentScore = ScoreManager.Instance.GetScore();
-        scoreText.text = "Score\n" + currentScore.ToString();
+        scoreText.text = "Score\n" + "<size=70>" + currentScore.ToString() + "</size>";
     }
 }
